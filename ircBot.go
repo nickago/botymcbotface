cb68d31d3e132d4ab176d0d6a3c5ed56656ba0ac
nickago <ngonella@calpoly.edu>
2016-05-27 21:02:56 -0700

    Added error checking to SendMessage

--- ircBot.go
+++ ircBot.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-	"fmt"
-  "io"
-	"net"
-  "os"
-  "strings"
+    "fmt"
+    "io"
+    "net"
+    "os"
+    "strings"
 )
 
 const USERNAME string = "attila_the_bot"
 
 func main() {
-  var connData [1024]byte
+    var connData [1024]byte
 
-	addr, err := net.ResolveTCPAddr("tcp", "chat.freenode.net:6667")
-  checkError(err)
+    addr, err := net.ResolveTCPAddr("tcp", "chat.freenode.net:6667")
+    checkError(err)
 
-  conn, err := net.DialTCP("tcp", nil, addr)
-  checkError(err)
+    conn, err := net.DialTCP("tcp", nil, addr)
+    checkError(err)
 
-  conn.Write([]byte(fmt.Sprintf("NICK %s\r\n", USERNAME)))
-	conn.Write([]byte(fmt.Sprintf("USER %s 0 * :(Here lies %s)\r\n", USERNAME, USERNAME)))
+    conn.Write([]byte(fmt.Sprintf("NICK %s\r\n", USERNAME)))
+    conn.Write([]byte(fmt.Sprintf("USER %s 0 * :(Here lies %s)\r\n", USERNAME, USERNAME)))
 
-  for {
-    n, err := conn.Read(connData[0:])
-    if err == io.EOF {
-      println("Connection Terminated")
-      os.Exit(1)
+    for {
+        n, err := conn.Read(connData[0:])
+        if err == io.EOF {
+            println("Connection Terminated")
+            os.Exit(1)
+        }
+        dataString := string(connData[:n])
+
+        for _, line := range strings.Split(dataString, "\n") {
+            go handleConnection(conn, line)
+        }
+
+        println(dataString)
+        println("----------------------------------------------")
     }
-    dataString := string(connData[:n])
-
-    for _, line := range strings.Split(dataString, "\n") {
-      go handleConnection(conn, line)
-    }
-
-    println(dataString)
-    println("----------------------------------------------")
-  }
 }
 
 func checkError(err error) {
@@ -44,27 +44,36 @@ func checkError(err error) {
         fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
         os.Exit(1)
     }
-  }
+}
 
 func handleConnection(conn *net.TCPConn, dataString string) {
-  data := strings.Split(dataString, " ")
+    data := strings.Split(dataString, " ")
 
-  if data[0] == "PING" {
-    conn.Write([]byte(fmt.Sprintf("PONG %s\r\n", data[1])))
-  }
+    if data[0] == "PING" {
+        conn.Write([]byte(fmt.Sprintf("PONG %s\r\n", data[1])))
+    }
 
-  if len(data) > 1 && (data[1] == "376" || data[1] == "422") {
-    conn.Write([]byte(fmt.Sprintf(":%s JOIN ##whitehat\r\n", USERNAME)))
-    conn.Write([]byte(fmt.Sprintf(":%s PRIVMSG ##whitehat :I'm Alive\r\n", USERNAME)))
-  }
+    if len(data) > 1 && (data[1] == "376" || data[1] == "422") {
+        conn.Write([]byte(fmt.Sprintf(":%s JOIN ##whitehat\r\n", USERNAME)))
+        sendMessage(conn, "##whitehat", "I'm Alive");
+    }
 
-  if len(data) > 1 && data[1] == "QUIT" {
-    os.Exit(1)
-  }
+    if len(data) > 1 && data[1] == "QUIT" {
+        os.Exit(1)
+    }
 
-  if len(data) > 1 && data[1] == "PRIVMSG" && data[2] == USERNAME {
-    println("Stuff")
-    sender := strings.Split(data[0], "!")[0]
-    conn.Write([]byte(fmt.Sprintf(":%s PRIVMSG %s :This isn't the turing test.", USERNAME, sender)))
-  }
+    if len(data) > 1 && data[1] == "PRIVMSG" && data[2] == USERNAME {
+        sender := strings.Split(data[0], "!")[0]
+        handlePM(conn, sender[1:], data[3][1:])
+    }
+}
+
+func sendMessage(conn *net.TCPConn, target string, msg string) {
+    msgString := fmt.Sprintf(":%s PRIVMSG %s :%s\r\n", USERNAME, target, msg))
+    _, err = conn.Write([]byte(msgString)
+    return err;
+}
+
+func handlePM error (conn *net.TCPConn, target string, cmd string) {
+    sendMessage(conn, sender, "This isn't the turing test")
 }
